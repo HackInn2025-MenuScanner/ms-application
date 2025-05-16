be285638e712aa3f03aa5007d9e9f847dc39dbfa
FloG99 <goeth-florian@freenet.de>
2025-05-16 17:43:18 +0200

    port 80

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,9 +1,24 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter()
+	},
+	compilerOptions: {
+		runes: true
+	},
+	vitePlugin:{
+		dynamicCompileOptions({filename}){
+		  if (filename.includes("node_modules")) {
+			return {runes: undefined}
+		  }
+		}
+	}
 };
 
 export default config;
