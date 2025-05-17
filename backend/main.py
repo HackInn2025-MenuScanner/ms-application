9614005ff7fc416d0551869375c2cd7acc9740ce
Demyan Kurbatov <kurbat02@ads.uni-passau.de>
2025-05-17 05:19:59 +0200

    Add CORS middleware to FastAPI application

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 load_dotenv()
 from controller.root import router
 
@@ -9,6 +11,14 @@ app = FastAPI(
     version="1.0.0",
     docs_url="/docs",
     redoc_url="/redoc"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
